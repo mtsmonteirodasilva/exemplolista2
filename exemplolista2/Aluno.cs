f282c4ce635b5d516642bbeea551e6a8e28b7b1b
Matheus Monteiro <matheus2016monteiro@gmail.com>
2023-07-31 16:46:17 -0400

    Primeiro commit

---
+++ exemplolista2/Aluno.cs
@@ -0,0 +1,20 @@
+﻿public class Aluno
+{
+    public int Matricula { get; set; }
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public DateOnly DataNascimento { get; set; }
+
+    public Aluno()//SOBRECARGA
+    {
+
+    }
+
+    public Aluno(int matricula, string nome, string cpf, DateOnly dataNascimento)
+    {
+        this.Matricula = matricula;
+        this.Nome = nome;
+        this.Cpf = cpf;
+        this.DataNascimento = dataNascimento;
+    }   
+}
---
+++ exemplolista2/Program.cs
@@ -0,0 +1,18 @@
+﻿Aluno aluno1 = new Aluno();
+aluno1.Nome = "Elias";
+aluno1.Cpf = "982-383-930-65";
+aluno1.Matricula = 10;
+
+Aluno aluno2 = new Aluno(11, "Matheus", "065-021-762-40", new DateOnly(2006, 11, 17));
+
+List<Aluno> alunos = new List<Aluno>();
+{
+    alunos.Add(aluno1);
+    alunos.Add(aluno2);
+}
+
+foreach(Aluno a in alunos)
+{
+    aluno1.Nome = "José";
+    Console.WriteLine($"O nome é: {a.Nome} o cpf é: {a.Cpf} e sua matricula é{a.Matricula}");
+}
